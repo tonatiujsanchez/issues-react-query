4eaf68dacf992034c8d9db78da8cb15a64681060
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2023-10-21 19:22:59 -0600

    Llamada a la API api.github implementado

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,8 +1,29 @@
-import { IssueList } from '../components/IssueList';
-import { LabelPicker } from '../components/LabelPicker';
+import { useQuery } from '@tanstack/react-query'
+import { githubApi } from '../../api'
 
+import { IssueList } from '../components/IssueList'
+import { LabelPicker } from '../components/LabelPicker'
+
+import { Label } from '../interface'
+
+
+const getLabels = async():Promise<Label[]> => {
+  const { data } = await githubApi<Label[]>('/labels')
+  return data
+}
 
 export const ListView = () => {
+
+  
+  const labelsQuery = useQuery({
+    queryKey: ['labels'],
+    queryFn: getLabels,
+    refetchOnWindowFocus: false,  
+  })
+
+
+
+
   return (
     <div className="row mt-5">
       
